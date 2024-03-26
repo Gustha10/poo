21df49f344d90a0d7c4f499cc5468595d1db2913
202311900028 <gusthavo.vidal@estudante.ifto.edu.br>
2024-03-26 08:06:06 -0300

    oi

---
+++ teste.py
@@ -0,0 +1,22 @@
+'''def validaemail(email)
+
+    try:
+        if email.count('@') == 1 and email.split("@")[1].count(".") >= 1: return True
+        else: return False
+    except: return False'''
+
+def calcpot (a,b):
+    R= x ** y
+    return R
+
+x= 7
+y= 9
+
+R=(x,y)
+print("Resultado:", R )
+
+giytg
+
+
+
+
